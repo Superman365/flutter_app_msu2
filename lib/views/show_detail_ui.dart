392df333af0d8ecdb77a4437a0e1779a68bcde01
superman365 <suphawetc@gmail.com>
2022-03-11 10:02:24 +0700

    Code V1.1 : assets plus

---
+++ lib/views/show_detail_ui.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShowdetailUI extends StatefulWidget {
+  const ShowdetailUI({Key? key}) : super(key: key);
+
+  @override
+  State<ShowdetailUI> createState() => _ShowdetailUIState();
+}
+
+class _ShowdetailUIState extends State<ShowdetailUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/views/splash_screen_ui.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SplashscreenUI extends StatefulWidget {
+  const SplashscreenUI({Key? key}) : super(key: key);
+
+  @override
+  State<SplashscreenUI> createState() => _SplashscreenUIState();
+}
+
+class _SplashscreenUIState extends State<SplashscreenUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
